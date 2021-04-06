342763c9bee1d60079413d4e78526818f7e2be83
Taylor Dean <taylordean787@gmail.com>
2021-04-06 09:14:43 -0500

    template setup

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -3,6 +3,8 @@
   <input type="text" v-model="city" placeholder="City" />
   <button @click="submitCity">enter city</button>
   <button @click="getLocation">Get Coords</button>
+  <h3>{{ currentTemp }}</h3>
+  {{ info }}
 </template>
 
 <script>
@@ -11,8 +13,10 @@ export default {
   name: "Weather",
   data() {
     return {
-      city: "San Antonio",
-      key: process.env.WEATHER_APIKEY,
+      city: "San-Antonio",
+      key: process.env.VUE_APP_WEATHER_APIKEY,
+      info: null,
+      currentTemp: null,
     };
   },
   methods: {
@@ -27,13 +31,16 @@ export default {
       console.log("error");
     },
     submitCity() {
-      console.log("click");
-      console.log(process.env.WEATHER_APIKEY);
-      fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=6caa22c818debc357a212d85570225b5`
-      )
-        .then((res) => console.log(res))
-        .then((data) => console.log(data));
+      axios
+        .get(
+          `http://api.openweathermap.org/data/2.5/weather?q=San-Antonio&appid=${this.key}&units=imperial`
+        )
+        .then(
+          (response) => (
+            (this.info = response.data.weather),
+            (this.currentTemp = response.data.main)
+          )
+        );
     },
   },
 };
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+window.axios = require("axios");
+require("dotenv").config();
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .mount("#app");
